260e77e8793b7fb321d82db6932ad4fce213e29a
NKNehak <nknehak098@gmail.com>
2020-10-25 19:12:34 +0530

    Adding All Project Files

---
+++ Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+      public GameObject box_Prefab;
+    // Start is called before the first frame update
+    public void SpawnBox() {
+        GameObject box_Obj =Instantiate(box_Prefab);
+        Vector3 temp =transform.position;
+        temp.z=0f;
+
+        box_Obj.transform.position = temp;
+    }
+}
